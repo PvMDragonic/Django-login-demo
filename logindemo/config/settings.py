99547e432561f9f7a8e2a3750c1244cde1e7a23a
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2025-05-05 14:11:46 -0300

    'sign_up' app renamed to 'users'.

--- logindemo/config/settings.py
+++ logindemo/config/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'compressor',
     'landing',
-    'sign_up',
+    'users',
 ]
 
 MIDDLEWARE = [
--- logindemo/users/apps.py
+++ logindemo/users/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 class SignUpConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sign_up'
+    name = 'users'
     verbose_name = 'Registered Users'
