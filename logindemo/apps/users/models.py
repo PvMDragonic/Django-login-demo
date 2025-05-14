3c6a4645e3164fb7f3352d704f451dadd444d885
João Pedro Droval <55903030+PvMDragonic@users.noreply.github.com>
2025-05-14 16:54:18 -0300

    CustomUser rework to extend the correct classes.
    CustomUser was extending a generic model, which was incompatible with
    Django's built-in password managing stuff.

--- logindemo/apps/users/models.py
+++ logindemo/apps/users/models.py
@@ -1,14 +1,44 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, 
+    BaseUserManager, 
+    PermissionsMixin
+)
 
-class CustomUser(models.Model):
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, username, password = None, **extra_fields):
+        if not email:
+            raise ValueError("Users must have an email address.")
+        
+        if not username:
+            raise ValueError("Users must have a username.")
+        
+        email = self.normalize_email(email)
+        user = self.model(email = email, username = username, **extra_fields)
+        user.set_password(password) 
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username, password = None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        return self.create_user(email, username, password, **extra_fields)
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length = 50, unique = True)
-    password = models.CharField(max_length = 128)
     email = models.EmailField(unique = True)
     signup_date = models.DateTimeField(auto_now_add = True)
+    is_active = models.BooleanField(default = True)
+    is_staff = models.BooleanField(default = False)
+
+    objects = CustomUserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
 
     def __str__(self):
         return self.username
-    
+
     class Meta:
         verbose_name = "User"
         verbose_name_plural = "Users"
